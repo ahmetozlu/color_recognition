86b933990138dcfa7cd1d49e0864e1cf18f6d0cd
ahmetozlu <ahmetozlu93@gmail.com>
2018-07-21 01:07:14 +0300

    python2 to python3 upgrade

--- src/color_classification_image.py
+++ src/color_classification_image.py
@@ -1,8 +1,10 @@
-#-------------------------------------------------------------------------
-#--- Author         : Ahmet Ozlu
-#--- Mail           : ahmetozlu93@gmail.com
-#--- Date           : 8th July 2018 - before Google inside look 2018 :)
-#-------------------------------------------------------------------------
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------------
+# --- Author         : Ahmet Ozlu
+# --- Mail           : ahmetozlu93@gmail.com
+# --- Date           : 8th July 2018 - before Google inside look 2018 :)
+# -------------------------------------------------------------------------
 
 import cv2
 from color_recognition_api import color_histogram_feature_extraction
@@ -12,24 +14,31 @@ import os.path
 
 # read the test image
 source_image = cv2.imread('black_cat.jpg')
-prediction = "n.a."
+prediction = 'n.a.'
 
-#checking whether the training data is ready
-PATH='./training.data'
+# checking whether the training data is ready
+PATH = './training.data'
 
 if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
-    print "training data is ready, classifier is loading..."
+    print ('training data is ready, classifier is loading...')
 else:
-    print "training data is being created..."
-    open('training.data','w')
+    print ('training data is being created...')
+    open('training.data', 'w')
     color_histogram_feature_extraction.training()
-    print "training data is ready, classifier is loading..."
+    print ('training data is ready, classifier is loading...')
 
 # get the prediction
 color_histogram_feature_extraction.color_histogram_of_test_image(source_image)
-prediction = knn_classifier.main("training.data", "test.data")
-cv2.putText(source_image, "Prediction: " + prediction, (15, 45), cv2.FONT_HERSHEY_PLAIN, 3, 200)
+prediction = knn_classifier.main('training.data', 'test.data')
+cv2.putText(
+    source_image,
+    'Prediction: ' + prediction,
+    (15, 45),
+    cv2.FONT_HERSHEY_PLAIN,
+    3,
+    200,
+    )
 
 # Display the resulting frame
-cv2.imshow('color classifier',source_image)
-cv2.waitKey(0)
+cv2.imshow('color classifier', source_image)
+cv2.waitKey(0)		
--- src/color_classification_webcam.py
+++ src/color_classification_webcam.py
@@ -1,8 +1,10 @@
-#----------------------------------------------
-#--- Author         : Ahmet Ozlu
-#--- Mail           : ahmetozlu93@gmail.com
-#--- Date           : 31st December 2017 - new year eve :)
-#----------------------------------------------
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------
+# --- Author         : Ahmet Ozlu
+# --- Mail           : ahmetozlu93@gmail.com
+# --- Date           : 31st December 2017 - new year eve :)
+# ----------------------------------------------
 
 import cv2
 from color_recognition_api import color_histogram_feature_extraction
@@ -11,35 +13,43 @@ import os
 import os.path
 
 cap = cv2.VideoCapture(1)
-ret, frame = cap.read()
-prediction = "n.a."
+(ret, frame) = cap.read()
+prediction = 'n.a.'
 
-#checking whether the training data is ready
-PATH='./training.data'
+# checking whether the training data is ready
+PATH = './training.data'
 
 if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
-    print "training data is ready, classifier is loading..."
+    print ('training data is ready, classifier is loading...')
 else:
-    print "training data is being created..."
-    open('training.data','w')
+    print ('training data is being created...')
+    open('training.data', 'w')
     color_histogram_feature_extraction.training()
-    print "training data is ready, classifier is loading..."
+    print ('training data is ready, classifier is loading...')
 
-while(True):
+while True:
+
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    (ret, frame) = cap.read()
 
-    cv2.putText(frame, "Prediction: " + prediction, (15, 45), cv2.FONT_HERSHEY_PLAIN, 3, 200)
+    cv2.putText(
+        frame,
+        'Prediction: ' + prediction,
+        (15, 45),
+        cv2.FONT_HERSHEY_PLAIN,
+        3,
+        200,
+        )
 
     # Display the resulting frame
-    cv2.imshow('color classifier',frame)
+    cv2.imshow('color classifier', frame)
 
     color_histogram_feature_extraction.color_histogram_of_test_image(frame)
-    
-    prediction = knn_classifier.main("training.data", "test.data")
+
+    prediction = knn_classifier.main('training.data', 'test.data')
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
 cap.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()		
